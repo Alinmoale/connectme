46bf4a775798c5b036c086365aa71729f4207eb5
Alin Moale <moalealin15@gmail.com>
2024-08-24 16:53:00 +0300

    adding the users in db

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -16,6 +16,8 @@ import { Button } from "../../components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "../../lib/validation";
 import Loader from "../../components/ui/shared/Loader";
+import { Link } from "react-router-dom";
+import { createUserAccount } from "../../lib/appwrite/api";
 
 const SignupForm = () => {
   const isLoading = false;
@@ -32,10 +34,8 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser = await createUserAccount(values);
   }
   return (
     <Form {...form}>
@@ -119,6 +119,15 @@ const SignupForm = () => {
               "Sign up"
             )}
           </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Log in
+            </Link>
+          </p>
         </form>
       </div>
     </Form>
